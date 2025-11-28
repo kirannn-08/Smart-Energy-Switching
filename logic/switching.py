9d3820e863516b204a4e018e436a6479160df705
kirannn-08 <kiranvenkatravelli@gmail.com>
2025-11-28 16:47:58 +0530

    Initial commit

---
+++ logic/switching.py
@@ -0,0 +1,11 @@
+"""
+This file briefs about the switching logic for the 
+Smart Energy System. 
+
+skeletal code for test / updates will be made further as we progress in /
+
+This current logic decides priority automatically based on :
+- Load side (power_kw): how loads are more 'solar - friendly '
+- Weather the load can run on solar or no
+- Weather the load is containg any high power appliances
+"""
